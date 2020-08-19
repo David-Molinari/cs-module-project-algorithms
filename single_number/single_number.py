1ff569667fb738bf623187dc877eff6057f114b3
David Molinari <david.m.molinari@gmail.com>
2020-08-19 18:05:43 -0400

    Project progress

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,29 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
-    # Your code here
+    if len(arr) > 0:
+        arr.sort()
+        count = 0
+        for i in arr:
+            if count == 0:
+                if i != arr[count+1]:
+                    return i
+                else:
+                    count += 1
+            elif count == len(arr)-1:
+                if i != arr[count-1]:
+                    return i
+                else:
+                    return None
+            elif i != arr[count+1] and i != arr[count-1]:
+                return i
+            else:
+                count += 1
 
-    pass
-
+            
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
