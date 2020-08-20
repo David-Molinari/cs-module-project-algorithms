6f2b150c1f71cf9e322f4ae950aaa4efe723e971
David Molinari <david.m.molinari@gmail.com>
2020-08-19 20:48:49 -0400

    Project progress

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,11 +1,26 @@
 '''
-Input: a List of integers as well as an integer `k` representing the size of the sliding window
+Input: a List of integers as well as an integer `k` 
+representing the size of the sliding window
 Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
+    if len(nums) > 0:
+        count = 0
+        new_arr = []
+        for i in nums:
+            if len(nums) - count >= k:
+                largest_num = float('-inf')
+                for j in nums[count:count+k]:
+                    if j > largest_num:
+                        largest_num = j
+                new_arr.append(largest_num)
+                count += 1
+        return new_arr
 
-    pass
+
+
+
 
 
 if __name__ == '__main__':
